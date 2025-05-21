da66bcf0b551a3417b78ca3eed5f8400cca83430
kdsgd <1005018184@qq.com>
2025-05-22 00:59:28 +0800

    启用 Simbot 自动配置

--- src/main/java/com/OHYS/Destiny2Tools/Destiny2ToolsApplication.java
+++ src/main/java/com/OHYS/Destiny2Tools/Destiny2ToolsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableSimbot // 启用 Simbot 自动配置
+@EnableSimbot //  启用 Simbot 自动配置
 public class Destiny2ToolsApplication {
 
 	public static void main(String[] args) {
