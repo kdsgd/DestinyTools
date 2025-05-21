f0f19d3b94d2d3c0223dc345feae73cb2b631026
kdsgd <1005018184@qq.com>
2025-05-22 00:56:20 +0800

    优化pom文件；启用 Simbot 自动配置

--- src/main/java/com/OHYS/Destiny2Tools/Destiny2ToolsApplication.java
+++ src/main/java/com/OHYS/Destiny2Tools/Destiny2ToolsApplication.java
@@ -1,10 +1,12 @@
 package com.OHYS.Destiny2Tools;
 
 
+import love.forte.simboot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSimbot // 启用 Simbot 自动配置
 public class Destiny2ToolsApplication {
 
 	public static void main(String[] args) {
